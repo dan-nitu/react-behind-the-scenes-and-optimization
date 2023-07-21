af56753717610734b676c8e09576d126768c1f5a
Voodoo30fps <voodoo30fps@gmail.com>
2023-07-21 12:02:01 +0300

    react memo

--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -1,7 +1,9 @@
+import React from 'react';
+
 import MyParagraph from './MyParagraph';
 
 const DemoOutput = (props) => {
   console.log('DemoOutput RUNNING');
   return <MyParagraph>{props.show ? 'This is new!' : ''}</MyParagraph>;
 };
-export default DemoOutput;
+export default React.memo(DemoOutput);
