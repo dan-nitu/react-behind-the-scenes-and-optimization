7090a073ccab6fb3e0980059a7cd0f562df97ee0
Voodoo30fps <voodoo30fps@gmail.com>
2023-07-21 12:16:17 +0300

    useCallback with dependencies

--- src/App.js
+++ src/App.js
@@ -7,17 +7,25 @@ import './App.css';
 
 function App() {
   const [showParaghraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('APP RUNNING');
 
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph((prevShowParaghraph) => !prevShowParaghraph);
-  }, []);
+    if (allowToggle) {
+      setShowParagraph((prevShowParaghraph) => !prevShowParaghraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandle = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div className='app'>
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParaghraph} />
+      <Button onClick={allowToggleHandle}>Allow Toggle!</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
