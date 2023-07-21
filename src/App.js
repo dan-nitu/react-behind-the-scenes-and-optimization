a25e434ba9b2259d2c84c76fcf78cb6f0ac9723a
Voodoo30fps <voodoo30fps@gmail.com>
2023-07-21 11:00:06 +0300

    component updates in action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
+
+import Button from './components/UI/Button/Button';
 
 import './App.css';
 
 function App() {
+  const [showParaghraph, setShowParagraph] = useState(false);
+
+  console.log('APP RUNNING');
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParaghraph) => !prevShowParaghraph);
+  };
+
   return (
-    <div className="app">
+    <div className='app'>
       <h1>Hi there!</h1>
+      {showParaghraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
